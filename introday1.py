dd11accfd716c92dc74b4b6b1983d272f0214a2a
kindkrishna1999 <kindkrishna1999@gmail.com>
2026-05-13 14:42:14 +0545

    core python code pushed

---
+++ introday1.py
@@ -0,0 +1,101 @@
+#python:python is a high level object orient interpreted language.
+#field that python mainlly used in 
+#machine learning 
+#data science(numpy, tensor)
+# web development(django)
+#comment in python: comment is the feature in python that ignore the execution of code as whole line.
+#++two ways to comment code in python++++++++
+#1 single line comment : single line comment os done using hastag(#)
+#example
+# a=5
+# print(a)
+#shortcut key to comment in python is (ctrl+/)
+#2 multiline comment in python using doc-stringor triple single qoutes'''''' or double quotes""""""":
+"""
+# a=5
+# b=0
+# #print(a/b)
+# print(a)
+# print(b)
+ 
+ """
+# #variable: it is the container that is used to store value or data.
+# 1....................#camelcase: in camel case the first word is small and the second starting word SHOULD BE  is capital.
+# #example:
+# collegeName= "xaiver school"
+# print(collegeName)
+
+# 2................#snake_case: in snake case every word separated by underscore.
+# college_name="run school"
+# print(college_name)
+# #snake case is the best way to declare the variable
+
+
+#3......................pascal case : every words first letter should be capital
+#example:
+# CollgeName="ram"
+# print(CollgeName)
+
+
+
+
+
+
+#datatype in python: data is a set if instruction.
+#types of dara
+#1. Numeric data type: the data which is in the form of number or interger known as numeric data type.
+#a.interger(int): interger is the numerical value without including decimal value(1,2,3)
+
+# a,b=4,7
+# print(a,b)
+# print(a)
+# print(b)
+
+# #float: float is the data which is used in python to take approximate valure.
+# #example
+# #pi=3.14159 #exact value
+# pi=3.14 #approximate value
+# print(pi)
+
+
+
+
+# # a=3.15
+# # print(type(a))
+
+# a=5
+# #print(type(a)) type ko kam find garnae ho
+
+
+# a=123445543455555
+# print(type(a))
+
+# collgeName="ram school"
+# print(type(collgeName))
+
+
+#Home work
+#name address education gender age collegeName print all
+# name="Krishna Kumar Pariyar"
+# print("my name is :", name)
+# print(type(name))
+
+# address="Signamangal"
+# print("my address name is:",address)
+# print(type(address))
+
+# education="bachelor degree in Geology"
+# print("my education qualification is:",education)
+# print(type(education))
+
+# gender="male"
+# print("my gender is:", gender)
+# print(type(gender))
+
+# age=26, 
+# print("i am year old:", age)
+# print(type(age))
+
+collgeName = "Tri-chandra Multiple Campus"
+print("my college Name is:", collgeName )
+print(type(collgeName ))
---
+++ introday2.py
@@ -0,0 +1,210 @@
+#taking input from users
+
+# name = input("enter your name:")
+# print (name)
+
+# age = int(input("enter your age")) 
+# print(age)
+# print(type(age))                         
+
+
+# # age here age is show as sstringer but it is interger
+# address = input("enter your address")
+# print(address)
+
+# product_name = "Iphone 13 pro" 
+# price = 155000
+# color = "Black" 
+
+# output:
+# Product Name  = Iphone 13 pro
+# Product Price = 155000
+# Product Color = Black
+
+# product_name = input("enter your product name: ")
+# print("product name = ", product_name)
+
+
+# price = float(input("enter your price: "))
+# print("product price = ", price)
+
+# product_color = input("enter your product color: " )
+# print("product color = " , product_color)
+
+
+
+#2. WAP to calculate the area of rectangle taking length and breadth from user.
+# length = float(input("enter the value of length: "))
+# breath = float(input("enter the value of breath: "))
+# area = length * breath
+# print("the area of recentangle is: ", area )
+
+# ram = "16GB"
+# price = 120000
+
+# output:
+# Laptop Name = Dell XPS 13
+# RAM         = 16GB
+# Price       = 120000
+
+
+# category = input("enter your category: ")
+# print("category is :", category)
+
+# price = 30000
+# print(price)
+
+
+# radius = float(input("enter the value of radius: "))
+# print("the value of radius: ", radius)
+
+# pie = 3.153
+# print(pie)
+
+# areaOfCircle = pie * radius* radius
+# print(areaOfCircle)
+
+# write a program to find the area of triangle by taking data from user
+
+# base = float(input("enter the value of base: "))
+# height = float(input("enter the value of height: "))
+# areaOfTriangle = 0.5 * base * height
+# print(areaOfTriangle)
+
+
+
+
+# Q1.
+# product_name = "Iphone 13 pro" 
+# price = 155000
+# color = "Black" 
+
+# output:
+# Product Name  = Iphone 13 pro
+# Product Price = 155000
+# Product Color = Black
+
+# Q2. student_name = "Rahul"
+# age = 21
+# course = "BCA"
+
+# output:
+# Student Name = Rahul
+# Age = 21
+# Course = BCA
+
+
+# Q3. laptop_name = "Dell XPS 13"
+# ram = "16GB"
+# price = 120000
+
+# output:
+# Laptop Name = Dell XPS 13
+# RAM         = 16GB
+# Price       = 120000
+
+# Qn4.movie_name = "Inception"
+# rating = 9
+# year = 2010
+
+# Output:
+# Movie Name = Inception
+# Rating     = 9
+# Year       = 2010
+
+# movieName = input("enter the movies name: ")
+# print(movieName)
+
+# scale = float(input("enter the number: "))
+# print("rate of your movies is:", scale)
+
+# year = int(input("enter the year: "))
+# print("movies screening year:" , year)
+
+
+# output:
+# Laptop Name = Dell XPS 13
+# RAM         = 16GB
+# Price       = 120000
+
+# name = input("name of laptop")
+# print(name)
+
+# category = int (input("your category: "))
+# print("your ram is:" , category)
+
+
+# a = 4
+# b = 7
+# c = 6
+# print(a,"\n",b,"\n",c)
+
+
+
+
+
+
+# homework
+# question no 1
+# wap to find the area of a parallelogram by taking input from users
+# keynotes
+# input base, height
+# formula area =base *height same as rectangle concept but different shape name
+
+# base = float(input("enter the value of base: "))
+# height = float(input("enter the value of height: "))
+# area = base * height 
+# print(" area of a parallelogram: ", area)
+
+
+
+
+
+
+# question no 2
+# find the perimeter of a square by taking input from users
+# input side
+# formula perimeter = 4*side
+# single input , multiplication only
+
+# side=float(input("enter the value of side"))
+# perimeter = 4*side
+# print("perimeter of square:", perimeter)
+
+
+
+
+# question no 3
+# find the smple interest by taking input  frim user
+# input principal rate rate and Time
+# formula si=(p*t*r)/100
+# watch division carefully
+
+# principal = float(input("enter the value of :"))
+# time = int(input("enter the value of T:"))
+# rate = float(input("enter the value of R:"))
+# simpleInterest = (principal * time * rate) / 100
+# print("the value of Simple interest is1: ", simpleInterest)
+
+
+
+# wap to find the average of 3 number by taking input from users
+
+# input num1, num2, num3
+# fromula avg=(n1+n2+n3)/3 additon +division
+
+n1 = float(input("enter the value of n1: "))
+n2 = float(input("enter the value of n2: "))
+n3 = float(input("enter the value of n3: "))
+average = (n1 + n2 + n3) / 3
+print("the average vlaue of 3 number is:" , average)
+
+
+# conter kilometres to meter by taking input from users
+# input kilometers 
+# formula meter=km*1000
+# simple unit conversion, multiplication only
+
+# km = float(input("enter the value of Kilometer"))
+# meter = km*1000
+# print("the value of meter is", meter )
